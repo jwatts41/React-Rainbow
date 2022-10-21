f318a44174e3da5d7a6d24b13e2de7a3bff7e24d
jwatts41 <wattsjonovan@yahoo.com>
2022-10-21 18:25:11 -0400

    created a react rainbow

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
+
+function App(){
+    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    let colorMap = colors.map((color, i) => {
+      return (
+        <ColorBlock color={color} />
+      )
+    })
+    return (
+      <div className="App">
+      {colors.map((color, i) => 
+          <ColorBlock key= {color={color}} />
+      )}
+      {colorMap}
+  </div>
+  
+    )
+}
+
+export default App;
